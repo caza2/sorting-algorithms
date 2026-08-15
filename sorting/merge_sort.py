35e94f9e477a78ca16ecfb6e105f26f5f4c2c8c3
caza2 <antoine.cazanave@polytechnique.edu>
2026-08-15 16:54:35 +0200

    Implementation merge sort algorithm

---
+++ sorting/merge_sort.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+# Merge Sort. Time O(n log n). Space O(n)
+
+def merge_sort(arr: Optional[list[int]]) -> Optional[list[int]]:
+
+    if len(arr) <= 1:
+        return arr
+
+    def merge(arr1: list[int], arr2: list[int]) -> list[int]:
+        # Merges two sorted arrays
+        new_arr: list[int] = []
+        index_1, index_2 = 0, 0
+
+        while index_1 < len(arr1) and index_2 < len(arr2):
+            if arr1[index_1] < arr2[index_2]:
+                new_arr.append(arr1[index_1])
+                index_1 += 1
+            else:
+                new_arr.append(arr2[index_2])
+                index_2 += 1
+
+        while index_1 < len(arr1):
+            new_arr.append(arr1[index_1])
+            index_1 += 1
+
+        while index_2 < len(arr2):
+            new_arr.append(arr2[index_2])
+            index_2 += 1
+
+        return new_arr
+
+    return merge(merge_sort(arr[:len(arr)//2]), merge_sort(arr[len(arr)//2:]))
---
+++ tests/test_merge_sort.py
@@ -0,0 +1,34 @@
+import random
+from sorting.merge_sort import merge_sort
+
+def run_tests():
+    test_cases: list[tuple[list[int], list[int]]] = [
+        ([3, 1, 2], [1, 2, 3]),
+        ([], []),
+        ([1], [1]),
+        ([5, 4, 3, 2, 1], [1, 2, 3, 4, 5]),
+        ([2, 2, 1], [1, 2, 2]),
+        ([-3, 2, -1, 0], [-3, -1, 0, 2]),
+    ]
+    # Add 100 random test cases
+    for _ in range(100):
+        size: int = random.randint(0, 100)
+        nums: list[int] = [random.randint(-1000, 1000) for _ in range(size)]
+
+        test_cases.append((nums, sorted(nums)))
+
+    for i, (input_list, expected) in enumerate(test_cases, start=1):
+        result = merge_sort(input_list.copy())
+
+        assert result == expected, (
+            f"Test {i} failed\n"
+            f"Input:    {input_list}\n"
+            f"Expected: {expected}\n"
+            f"Got:      {result}"
+        )
+
+    print(f"All {len(test_cases)} tests passed!")
+
+
+if __name__ == "__main__":
+    run_tests()
